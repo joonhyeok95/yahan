338eb69fc633b8f7eb67c4e247ce28a231ca1880
임준혁 <dkttkemf@gmail.com>
2022-01-22 15:16:35 +0900

    스프링부트&타임리프 파일업로드 구현

---
+++ src/main/java/com/yehan/web/vo/FileVO.java
@@ -0,0 +1,23 @@
+package com.yehan.web.vo;
+
+import java.util.Arrays;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FileVO {
+	String name;
+	String filename;
+	String url;
+	String content;
+	MultipartFile[] multiFiles;
+	@Override
+	public String toString() {
+		return "FileVO [name=" + name + ", filename=" + filename + ", url=" + url + ", content=" + content
+				+ ", multiFiles=" + Arrays.toString(multiFiles) + "]";
+	}
+}
